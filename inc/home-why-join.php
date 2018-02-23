8a35d603c3d5e925982d4744fab71025f4be42ea
moxdev <moxdev1@gmail.com>
2018-02-23 10:00:59 -0500

    bout to blow up section

--- inc/home-why-join.php
+++ inc/home-why-join.php
@@ -7,50 +7,54 @@
 
 function promo_box_club_home_why_join_section() {
 	if ( function_exists( 'get_field' ) ) {
-    if( have_rows( 'why_join_section_information' ) ):
-      $why_join_section_title    = get_sub_field( 'why_join_section_title' ); ?>
+    if( have_rows( 'why_join_section_information' ) ): ?>
 
-    <section class="home-why-join">
-      <div class="wrapper">
+      <?php while( have_rows('why_join_section_information') ): the_row();
+        $why_join_background_image = get_sub_field( 'why_join_background_image' );
+        $why_join_first_column     = get_sub_field( 'why_join_first_column' );
+        $why_join_second_column    = get_sub_field( 'why_join_second_column' );
+        $why_join_featured_image   = get_sub_field( 'why_join_featured_image' );
+        $why_join_button_text      = get_sub_field( 'why_join_button_text' );
+        $why_join_button_page_link = get_sub_field( 'why_join_button_page_link' ); ?>
 
-        <?php while( have_rows('why_join_section_information') ): the_row();
-          $why_join_first_column     = get_sub_field( 'why_join_first_column' );
-          $why_join_second_column    = get_sub_field( 'why_join_second_column' );
-          $why_join_featured_image   = get_sub_field( 'why_join_featured_image' );
-          $why_join_button_text      = get_sub_field( 'why_join_button_text' );
-          $why_join_button_page_link = get_sub_field( 'why_join_button_page_link' ); ?>
-
-        <div class="inner-wrapper">
-
-          <?php if( $why_join_first_column ) : ?>
-            <div class="flex-column first-col-wrapper">
-              <?php echo $why_join_first_column; ?>
-            </div>
+        <section class="home-why-join">
+          <?php if( $why_join_background_image ) : ?>
+            <figure class="why-join-feature-img">
+              <img src="<?php echo esc_url( $why_join_background_image['sizes']['thumbnail'] ); ?>" alt="<?php echo esc_attr( $why_join_background_image['alt'] ); ?>" description="<?php echo esc_attr( $why_join_background_image['description'] ); ?>">
+            </figure>
           <?php endif; ?>
 
-          <?php if( $why_join_second_column ) : ?>
-            <div class="flex-column second-col-wrapper">
-              <?php echo $why_join_second_column; ?>
+          <div class="wrapper">
+            <div class="inner-wrapper">
+
+              <?php if( $why_join_first_column ) : ?>
+                <div class="flex-column first-col-wrapper">
+                  <?php echo $why_join_first_column; ?>
+                </div>
+              <?php endif; ?>
+
+              <?php if( $why_join_second_column ) : ?>
+                <div class="flex-column second-col-wrapper">
+                  <?php echo $why_join_second_column; ?>
+                </div>
+              <?php endif; ?>
+
+              <?php if( $why_join_featured_image ) : ?>
+                <div class="flex-column img-wrapper">
+                  <img src="<?php echo esc_url( $why_join_featured_image['sizes']['thumbnail'] ); ?>" alt="<?php echo esc_attr( $why_join_featured_image['alt'] ); ?>" description="<?php echo esc_attr( $why_join_featured_image['description'] ); ?>">
+                </div>
+              <?php endif; ?>
+
+              <?php if( $why_join_button_text ) : ?>
+                <div class="flex-column button-wrapper">
+                  <a class="btn" href="<?php echo wp_kses_post( $why_join_button_page_link ); ?>"><?php echo wp_kses_post( $why_join_button_text ); ?></a>
+                </div>
+              <?php endif; ?>
+
             </div>
-          <?php endif; ?>
-
-          <?php if( $why_join_featured_image ) : ?>
-            <div class="flex-column img-wrapper">
-              <img src="<?php echo esc_url( $why_join_featured_image['sizes']['thumbnail'] ); ?>" alt="<?php echo esc_attr( $why_join_featured_image['alt'] ); ?>" description="<?php echo esc_attr( $why_join_featured_image['description'] ); ?>">
-            </div>
-          <?php endif; ?>
-
-          <?php if( $why_join_button_text ) : ?>
-            <div class="flex-column button-wrapper">
-              <a class="btn" href="<?php wp_kses_post( $why_join_button_page_link ); ?>"><?php echo wp_kses_post( $why_join_button_text ); ?></a>
-            </div>
-          <?php endif; ?>
-
-        </div>
-        <?php endwhile; ?>
-
-      </div>
-    </section>
+          </div>
+        </section>
+      <?php endwhile; ?>
 
     <?php endif;
 	}
